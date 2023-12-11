ece0d60814674476ba9c83f9472727ebc7c4aa5a
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-11 20:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const extendHex = (shortHex) => {
-  // write your code here
-	shortHex=shortHex.replace(#,'');
-     if(shortHex===3){
-	shortHex=shortHex.split('').map(char => char+char).join('');
-	 }
-	return "#"+shortHex.toUpperCase();
-};
+  // Remove '#' if present
+  shortHex = shortHex.replace('#', '');
+
+  // Ensure the shortHex has 3 characters
+  if (shortHex.length === 3) {
+    // Extend each color component to 2 characters
+    shortHex = shortHex.split('').map(char => char + char).join('');
+  }
+
+  // Convert to uppercase and return the extended hex code with '#'
+  return '#' + shortHex.toUpperCase();
+}
+
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
