f27b609f9a2b13c3f7fc244c0c60d9b412ae6434
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-11 20:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex=shortHex.replace(#,'');
+     if(shortHex===3){
+	shortHex=shortHex.split('').map(char => char+char).join('');
+	 }
+	return "#"+shortHex.toUpperCase();
 };
 
 // Do not change the code below.
